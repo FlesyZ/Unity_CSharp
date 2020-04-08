25c7b5a84505b5a791b4dad80ecb4e3673b7f2fd
FlesyZ <anton.nnh@gmail.com>
2020-04-08 15:31:00 +0800

    認識欄位

---
+++ Hello World/Assets/Class1.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+// x : MonoBehaviour -> script to Unity object
+public class Class1 : MonoBehaviour
+{
+    [Header("汽車")]
+    public Its_a_Car car01;
+    public Its_a_Car car02;
+
+    // events (color in blue)
+    //  Start - run one time at the begin --> initialization
+    private void Start()
+    {
+        int a = 2, b = 3, c = 5, d = 7, I = a << c, i = I >> 1;
+        char[] chars = { '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0', '0' };
+        int[] order =
+        {
+            I + b + c,
+            I + i + a + b,
+            I + i + b * 4,
+            I + i + a * 2 * b,
+            I + i +(a + b)* 3,
+            I / a,
+            I + c * 4 + b,
+            I + i + b * c,
+            I + i + b * d - b,
+            I + i + a * c + 2,
+            I + i +(a * a),
+            I / a + 1,
+        };
+        for (int k = 0; k < order.Length; k++)
+        {
+            chars[k] = (char)order[k];
+        }
+        print(string.Join("", chars));
+
+        // get
+        print("一號汽車的容量: " + car01.CC);
+        print("二號汽車的品牌: " + car02.brand);
+
+        // set
+        car01.CC = 4444;
+        car01.weight = 360.0f;
+        car01.brand = "TOYOTA";
+        car01.brake = true;
+    }
+}
---
+++ Hello World/Assets/Its_a_Car.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+// post script(//), summary(///)
+// .cs in project = blueprint, .cs in object = object
+
+/// define the object (car)
+
+public class Its_a_Car : MonoBehaviour
+{
+    // field(type x;) - storage a object
+    // public = access & show on Inspector, private = Not public
+    // data type: cc(int), weight(float), brand(string), brake(bool)
+    // default int = 0, float = 0, string = "", bool = false
+    // Attribute( [attribute(val)] ) = field support in Unity
+    // Title = [Header("str")], Tips = [Tooltip("str")]
+    
+    [Header("汽車屬性調整")]
+    /// <summary>
+    /// 汽車容量 (單位: cc)
+    /// </summary>
+    [Tooltip("汽車容量"), Range(500, 5000)]
+    public int CC = 2000;
+    /// <summary>
+    /// 汽車重量 (單位: t)
+    /// </summary>
+    [Tooltip("汽車重量")]
+    public float weight = 102.4f;
+    /// <summary>
+    /// 汽車品牌
+    /// </summary>
+    [Tooltip("汽車品牌")]
+    public string brand = "BMW";
+    /// <summary>
+    /// 是否煞車
+    /// </summary>
+    [Tooltip("是否煞車")]
+    public bool brake;
+}
